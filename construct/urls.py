cb6419c4d956e144db69916e31913b3beefa6780
DanilaBeliakov <danilabelyakoff@yandex.ru>
2023-04-04 22:45:39 +0300

    Base meetings functionality

--- construct/urls.py
+++ construct/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index_construct, name='index_construct'),
+    re_path('meeting/', views.meeting_view, name='meeting_view'),
 ]
--- construct/views.py
+++ construct/views.py
@@ -8,12 +8,19 @@ import datetime
 from django.utils.dateparse import parse_datetime
 import sys
 import os
+from .models import meetings
 sys.path.append(os.getcwd())
 
 def index_construct(request):
-
+    all = meetings.all_elements(house=request.session['house_id'])
     return render(
             request,
             'index_construct.html',
-            context = {},
+            context = {'all':all},
     )
+
+
+def meeting_view(request):
+    meeting_id = request.GET.get('id')
+    meeting = meetings.objects.get(meeting_id=meeting_id)
+    return render(request, 'meeting.html', context={'meeting': meeting})
