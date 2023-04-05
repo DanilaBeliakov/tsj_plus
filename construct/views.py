2c5ff7daa21c8ff9f7994994a5b3701ba990c8e5
DanilaBeliakov <danilabelyakoff@yandex.ru>
2023-04-05 14:30:50 +0300

    DOCX Template generation

--- construct/views.py
+++ construct/views.py
@@ -1,8 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
-
-from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponseNotFound
 import datetime
 from django.utils.dateparse import parse_datetime
@@ -10,6 +6,7 @@ import sys
 import os
 from .models import meetings
 sys.path.append(os.getcwd())
+from docxtpl import DocxTemplate
 
 def index_construct(request):
     all = meetings.all_elements(house=request.session['house_id'])
@@ -23,4 +20,13 @@ def index_construct(request):
 def meeting_view(request):
     meeting_id = request.GET.get('id')
     meeting = meetings.objects.get(meeting_id=meeting_id)
+    generate_docx()
     return render(request, 'meeting.html', context={'meeting': meeting})
+
+
+def generate_docx():
+    doc = DocxTemplate("/home/d0nald/PycharmProjects/TSH_Site/tsjsite/construct/template.docx")
+    context = {'address': 'ул. Пушкина, дом Колотушкина'}
+    doc.render(context)
+    doc.save('example.docx')
+    return 1
