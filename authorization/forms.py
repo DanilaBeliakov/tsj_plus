e84175db2647c5e5dc36b1429acf82c45acc50ff
DanilaBeliakov <danilabelyakoff@yandex.ru>
2023-04-13 17:01:22 +0300

    Improved sighup

--- authorization/forms.py
+++ authorization/forms.py
@@ -3,14 +3,13 @@ from .models import users
 from django.contrib.auth.forms import UserCreationForm
 
 class SignUpForm(forms.Form):
-    full_name = forms.CharField(label='Имя:', widget=forms.TextInput(attrs={"class":"myfield", 'style':'width: 400px; height: 40px;'}))
-    email = forms.CharField(label='Почта:', widget=forms.TextInput(attrs={"class":"myfield", 'style':'width: 400px; height: 40px;'}))
-    address = forms.CharField(label='Полный адрес дома:', widget=forms.TextInput(attrs={"class":"myfield", 'style':'width: 400px; height: 40px;'})) # help_text='Город, улица, дом'
-    flat_number = forms.CharField(label='Номер квартиры:', widget=forms.TextInput(attrs={"class":"myfield", 'style':'width: 400px; height: 40px;'})) # help_text='Город, улица, дом'
-    password_first = forms.CharField(label='Пароль:', widget=forms.PasswordInput(attrs={"class":"myfield", 'style':'width: 400px; height: 40px;'}))
-    password_second = forms.CharField(label='Подтверждение пароля:', widget=forms.PasswordInput(attrs={"class":"myfield", 'style':'width: 400px; height: 40px;'}))
-    flat_area = forms.CharField(label='Площадь личной квартиры:', widget=forms.TextInput(attrs={"class":"myfield", 'style':'width: 400px; height: 40px;'}))
-    flat_share = forms.CharField(label='Доля в личной квартире:', widget=forms.TextInput(attrs={"class":"myfield", 'style':'width: 400px; height: 40px;'}))
+    full_name = forms.CharField(label='Имя:', widget=forms.TextInput(attrs={"class":"myfield", 'style':'width: 400px; height: 40px;', 'name': 'full_name', 'pattern': "[а-яА-Я ]*$", 'title': "Только кириллица и пробелы", 'minlength':'10', 'maxlength': '100',}))
+    email = forms.CharField(label='Почта:', widget=forms.TextInput(attrs={"class":"myfield", 'style':'width: 400px; height: 40px;', 'name': 'email', 'pattern': "[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$", 'title': "Пример: example@example.com"}))
+    address = forms.CharField(label='Полный адрес дома:', widget=forms.TextInput(attrs={"class":"myfield", 'style':'width: 400px; height: 40px;', 'pattern': "^[а-яА-Я ,.\d\/]*$", 'title': 'Только кириллица, цифры и символы . и ,', 'minlength': '10', 'maxlength': '100'}))
+    flat_number = forms.CharField(label='Номер квартиры:', widget=forms.TextInput(attrs={"class":"myfield", 'style':'width: 400px; height: 40px;', 'pattern': "^[\d]*$", 'maxlength': 10, 'minlength': '1'}))
+    password_first = forms.CharField(label='Пароль:', widget=forms.PasswordInput(attrs={"class":"myfield", 'style':'width: 400px; height: 40px;', 'minlength': '8', 'maxlength': '16', 'title': 'Длина пароля от 8 до 16 символов'}))
+    flat_area = forms.CharField(label='Площадь личной квартиры:', widget=forms.TextInput(attrs={"class":"myfield", 'style':'width: 400px; height: 40px;', 'pattern': "^[\d.]*$", 'title': 'Можно вводить только цифры и символ  точки'}))
+    flat_share = forms.CharField(label='Доля в личной квартире:', widget=forms.TextInput(attrs={"class":"myfield", 'style':'width: 400px; height: 40px;', 'pattern': "^[\d\/]*$", 'placeholder': 'Например, 1 или 1/3'}))
 
     class Meta:
         model = users
